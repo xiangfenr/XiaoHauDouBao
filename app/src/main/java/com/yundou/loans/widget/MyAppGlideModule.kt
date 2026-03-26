e92ca53594c9823c9e82e7127ea6587daf658c26
17611719616 <799252606@qq.com>
2026-03-26 12:46:01 +0800

    去掉app模块

--- app/src/main/java/com/yundou/loans/widget/MyAppGlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.yundou.loans.widget
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class MyAppGlideModule : AppGlideModule()
